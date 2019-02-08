6cd72294ec81c30d617adb0708efd741201cd849
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-02-08 17:36:55 +0100

    autentificacion hecha, tambien añadido skup autentificacion para home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+# añadimos esto depues de hacer scaffold with flats name and address, do not do a scaffold!
+protect_from_forgery with: :exception
+   before_action :authenticate_user!
 end
